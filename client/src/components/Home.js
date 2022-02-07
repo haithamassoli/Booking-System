c84a2d44b4e10b329909588d90795bd7725512ee
haithamassoli <haitham.b.assoli@gmail.com>
2022-02-07 05:25:52 +0200

    styling navbar and home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,12 +5,19 @@ function Home() {
   let navigate = useNavigate();
 
   return (
-    <div>
-      <h1>If you're looking for great burger</h1>
-      <button onClick={() => navigate("/booking")}>Booking Now</button>
+    <div className="flex justify-center text-center items-center flex-col">
+      <h1 className="text-2xl sm:text-5xl my-5">
+        If you're looking for great burger
+      </h1>
+      <button
+        className="py-3 px-5 rounded-xl bg-pink-500 text-white ronded"
+        onClick={() => navigate("/booking")}
+      >
+        Booking Now
+      </button>
       <img
         src="https://uploads.turbologo.com/uploads/design/hq_preview_image/965594/draw_svg20210507-22909-gc03x0.svg.png"
-        alt=""
+        alt="donut"
       />
     </div>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,15 +3,28 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="p-8 bg-red-700 flex justify-between items-center">
-      <NavLink to={"/"} className="cursor-pointer text-[2rem] text-white">
-        BurgerMaker
+    <div className="p-8 bg-pink-600 flex justify-between items-center">
+      <NavLink to={"/"} className="cursor-pointer text-2xl text-white">
+        <div className="flex items-center">
+          <h3 className="sm:block hidden">DonutMaker</h3>
+          <img
+            src="https://clipart-best.com/img/donut/donut-clip-art-35.png"
+            alt="donut"
+            width={50}
+          />
+        </div>
       </NavLink>
       <div>
-        <NavLink to={"/login"} className="cursor-pointer text-xl text-white">
-          login
+        <NavLink
+          to={"/login"}
+          className="cursor-pointer text-xl text-white hover:text-pink-200"
+        >
+          Login
         </NavLink>
-        <NavLink to={"/signup"} className="cursor-pointer text-xl text-white">
+        <NavLink
+          to={"/signup"}
+          className="ml-4 cursor-pointer text-xl text-white hover:text-pink-200"
+        >
           SignUp
         </NavLink>
       </div>
