583334cf6d74b76bc0c628895887346ca977a325
haithamassoli <haitham.b.assoli@gmail.com>
2022-02-07 18:48:44 +0200

    styling thank you page

--- client/src/components/Booking.js
+++ client/src/components/Booking.js
@@ -468,8 +468,9 @@ function Booking() {
                   type="text"
                   bsSize="lg"
                   placeholder="Name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-pink-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-pink-500 dark:focus:border-pink-500"
                   value={booking.name}
+                  required
                   onChange={(e) => {
                     setBooking({
                       ...booking,
@@ -486,11 +487,12 @@ function Booking() {
                   Your Number
                 </label>
                 <input
-                  type="text"
+                  type="number"
                   bsSize="lg"
                   placeholder="Phone Number"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-pink-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-pink-500 dark:focus:border-pink-500"
                   value={booking.phone}
+                  required
                   onChange={(e) => {
                     setBooking({
                       ...booking,
@@ -507,11 +509,12 @@ function Booking() {
                   Your Email
                 </label>
                 <input
-                  type="text"
+                  type="email"
                   bsSize="lg"
                   placeholder="Email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-pink-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-black dark:focus:ring-pink-500 dark:focus:border-pink-500"
                   value={booking.email}
+                  required
                   onChange={(e) => {
                     setBooking({
                       ...booking,
